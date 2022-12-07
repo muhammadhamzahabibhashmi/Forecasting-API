98162c52fee0ddbd5da5bbcfc69218bfd028e763
muhammadhamzahabibhashmi <75246708+muhammadhamzahabibhashmi@users.noreply.github.com>
2022-12-07 07:04:21 +0500

    Add files via upload

--- FrontEndApp.py
+++ FrontEndApp.py
@@ -25,7 +25,7 @@ if genre == 'Train':
             payload = (my_string.to_dict())
             payload2 = {"UserID":'245',"NumberOfEpochs":NumberOfEpochs,"FileName":str(uploaded_file.name)}
             payyl={"payload1":payload,"payload2":payload2}
-            resspoon = requests.post(url="http://192.168.18.81:5000/train/", json=payyl)
+            resspoon = requests.post(url="http://20.29.120.236/train/", json=payyl)
             st.write('Go to CheckStatus')
             # try:
             #     st.write("Done Training")
@@ -35,27 +35,30 @@ if genre == 'Train':
 elif genre == 'CheckStatus':
     if st.sidebar.button('Check Status Of Training'):
         payload2 = {"UserID":"245"}
-        asd2=requests.post(url="http://192.168.18.81:5000/Checktrain/", json=payload2)
+        asd2=requests.post(url="http://20.29.120.236/Checktrain/", json=payload2)
         st.write(json.loads(asd2.text))
 
 elif genre == 'Prediction':
     payload2 = {"UserID":"245"}
-    asd2=requests.post(url="http://192.168.18.81:5000/Checktrain/", json=payload2)
-    ListOfUsersModelsmodelss = json.loads(asd2.text)['ListOfUsersModels']
-    newListOfUsersModelsmodelss = []
-    for drw in ListOfUsersModelsmodelss:
-        if (json.loads(asd2.text)[drw]['Training']) == "Completed":
-            newListOfUsersModelsmodelss.append(drw)
-    if len(newListOfUsersModelsmodelss) > 0:
-        optionforstores = st.sidebar.selectbox('Select a model for predictions',newListOfUsersModelsmodelss)
-        ListOfUsersModels = json.loads(asd2.text)[optionforstores]['ListOfStores']
-        optionforstore = st.sidebar.selectbox('Select a store for predictions',ListOfUsersModels)
-        forecastDayysnumber = st.sidebar.number_input('Forecast Days', min_value=1, step=1)
-        if st.sidebar.button('Predictions'):
-            payyl = {"UserID":'245',"store":optionforstore,"days":forecastDayysnumber,"FileName":optionforstores}
-            asd=requests.post(url="http://192.168.18.81:5000/detect/", json=payyl)
-            json_load = json.loads(asd.text)
-            st.write(f"Loading Predictions For {optionforstore}")
-            st.write(json_load)
-    else:
-        st.write("No model available for prediction. Check status and wait if training is In Progress")
+    asd2=requests.post(url="http://20.29.120.236/Checktrain/", json=payload2)
+    try:
+        ListOfUsersModelsmodelss = json.loads(asd2.text)['ListOfUsersModels']
+        newListOfUsersModelsmodelss = []
+        for drw in ListOfUsersModelsmodelss:
+            if (json.loads(asd2.text)[drw]['Training']) == "Completed":
+                newListOfUsersModelsmodelss.append(drw)
+        if len(newListOfUsersModelsmodelss) > 0:
+            optionforstores = st.sidebar.selectbox('Select a model for predictions',newListOfUsersModelsmodelss)
+            ListOfUsersModels = json.loads(asd2.text)[optionforstores]['ListOfStores']
+            optionforstore = st.sidebar.selectbox('Select a store for predictions',ListOfUsersModels)
+            forecastDayysnumber = st.sidebar.number_input('Forecast Days', min_value=1, step=1)
+            if st.sidebar.button('Predictions'):
+                payyl = {"UserID":'245',"store":optionforstore,"days":forecastDayysnumber,"FileName":optionforstores}
+                asd=requests.post(url="http://20.29.120.236/detect/", json=payyl)
+                json_load = json.loads(asd.text)
+                st.write(f"Loading Predictions For {optionforstore}")
+                st.write(json_load)
+        else:
+            st.write("No model available for prediction. Check status and wait if training is In Progress")
+    except KeyError:
+        st.write("Train the model first")
